b9bdd7c4d3069d3533fd0e8260aca14aaa3d22c8
Vany <vany.ikuze@student.moringaschool.com>
2023-06-16 18:06:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+// Write your solution in this file! 
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+    return customerName = customerName. toUpperCase();
+}
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer = 'Jane'
+    leastFavoriteCustomer = 'bob';
+}
